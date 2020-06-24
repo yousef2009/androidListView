65c960b29abb7863b2a5f009a004060336edf98c
yousef <ye.hamouda@gmail.com>
2020-06-24 10:58:21 +0300

    change country id

--- app/src/main/java/com/example/listviewinterface/MainActivity.java
+++ app/src/main/java/com/example/listviewinterface/MainActivity.java
@@ -27,15 +27,15 @@ public class MainActivity extends AppCompatActivity {
 
         data.add(new Country(1, R.drawable.ic_pakistan, "Pakistan",
                 "Population: 203,382,058", "Area: 796,095 km2"));
-        data.add(new Country(1, R.drawable.ic_afghanistan, "Afghanistan",
+        data.add(new Country(2, R.drawable.ic_afghanistan, "Afghanistan",
                 "Population: 25,500,100", "Area: 652,090 km2"));
-        data.add(new Country(1, R.drawable.ic_india, "India",
+        data.add(new Country(3, R.drawable.ic_india, "India",
                 "Population: 1,241,610,000", "Area: 3,387,260 km2"));
-        data.add(new Country(1, R.drawable.ic_iran, "Iran",
+        data.add(new Country(4, R.drawable.ic_iran, "Iran",
                 "Population: 77,288,000", "Area: 1,648,200 km2"));
-        data.add(new Country(1, R.drawable.ic_china, "China",
+        data.add(new Country(5, R.drawable.ic_china, "China",
                 "Population: 1,363,350,000", "Area: 9,640,820 km2"));
-        data.add(new Country(1, R.drawable.ic_america, "United State",
+        data.add(new Country(6, R.drawable.ic_america, "United State",
                 "Population: 317,706,00", "Area: 9,629,090 km2"));
 
 
