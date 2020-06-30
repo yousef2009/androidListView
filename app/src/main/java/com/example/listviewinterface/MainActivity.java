523171ee8d9ab8741ea674ffdc85d6c32e57a47b
Yousef Hamouda <ye.hamouda@gmail.com>
2020-06-30 15:55:48 +0300

    new

--- app/src/main/java/com/example/listviewinterface/MainActivity.java
+++ app/src/main/java/com/example/listviewinterface/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
                 //view is the selected row
                 view.setBackgroundColor(getColor(R.color.selectedRowColor));
-                view.setActivated(true);
+     //           view.setActivated(true);
 
            //     Log.e("name = ", data.get(i).getTvName());
 
